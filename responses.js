345676b35a8dae5b145c3ca197ede13f872dc17d
SUKANT S <sukantsakthivel66@gmail.com>
2023-03-10 17:50:48 +0530

    Update responses.js

--- responses.js
+++ responses.js
@@ -1,19 +1,19 @@
 function getBotResponse(input) {
     //rock paper scissors
-    if (input == "rock") {
-        return "paper";
-    } else if (input == "paper") {
-        return "scissors";
-    } else if (input == "scissors") {
-        return "rock";
+    if ((input == "Need some help" ) || (input == "Have some issues in the product" )){
+        return "Please provide your ID";
+    } else if (input == "12233445") {
+        return "What was the Problem You have face with the product";
+    } else if ((input == "Some parts have missed")||(input == "colour mismatched")||(input == "Colour mismatched")) {
+        return "Are you willing to Exchange Return the product or Cancel the process ";
     }
-
-    // Simple responses
-    if (input == "hello") {
-        return "Hello there!";
-    } else if (input == "goodbye") {
-        return "Talk to you later!";
-    } else {
+    else if ((input == "Exchange")||(input == "Return")) {
+        return "Congratulations!!!! Your request Accepted";
+    }
+    else if (input == "Cancel") {
+        return "Ok, Done. Your query is Noted ";
+    }
+    else {
         return "Try asking something else!";
     }
 }
